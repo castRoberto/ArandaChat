db6041d7ee43f5416724c089bd99a25eb029d818
rcastro <rcastro@cotecmar.com>
2022-07-31 04:55:31 -0500

    Se crea entry point de la aplicacion y utilidad para manejo de archivos json

---
+++ Source/AppArandaChat/Src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @brief Entry point for chat application
+ *
+ * @date 31/07/2022
+ * @authors robertoca07@hotmail.com
+ * @name main.cpp
+ */
+
+int main(int argc_p, char *argv_p[]) {
+
+
+	return 0;
+}
+
+
